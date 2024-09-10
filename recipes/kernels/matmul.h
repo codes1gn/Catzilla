4cbd5747725f5b43d3fe68a51fb480e4a8efe7cb
Albert.Shi <codefisheng@gmail.com>
2024-09-10 16:09:10 +0800

    v8: vload/vstore in DMA part, lhs left untouched

--- recipes/kernels/matmul.h
+++ recipes/kernels/matmul.h
@@ -599,7 +599,8 @@ void catzilla_matmul_v6(int M, int N, int K, float alpha, float *A, float *B,
       <<<gridDim, blockDim>>>(M, N, K, alpha, A, B, beta, C);
 }
 
-// XOR SWIZZLE 
+// flatten copy
+// TODO: xor-swizzle
 template <const int M_TILE, const int N_TILE, const int K_TILE,
           const int X_THREAD, const int Y_THREAD>
 __global__ void _catzilla_matmul_v7(int M, int N, int K, float alpha,
@@ -714,6 +715,7 @@ void catzilla_matmul_v7(int M, int N, int K, float alpha, float *A, float *B,
 }
 
 // + vload vstore 
+// TODO: let lhs also vectorise, currently only apply on rhs and out
 template <const int M_TILE, const int N_TILE, const int K_TILE,
           const int X_THREAD, const int Y_THREAD>
 __global__ void _catzilla_matmul_v8(int M, int N, int K, float alpha,
@@ -721,25 +723,27 @@ __global__ void _catzilla_matmul_v8(int M, int N, int K, float alpha,
                                     float *out) {
   auto lhs_shape = make_coord(M, K);
   auto rhs_shape = make_coord(K, N);
+  auto rhs_shape_v = make_coord(K, CEIL_DIV(N, 4));
   auto out_shape = make_coord(M, N);
+  auto out_shape_v = make_coord(M, CEIL_DIV(N, 4)); // in float4 type, overall shape is M, N/4
 
   auto lhs_sm_tile_shape = Coord(M_TILE, K_TILE);
   auto rhs_sm_tile_shape = Coord(K_TILE, N_TILE);
-  auto out_sm_tile_shape = Coord(M_TILE, N_TILE);
+  auto rhs_sm_tile_shape_v = Coord(K_TILE, CEIL_DIV(N_TILE, 4));
+  auto out_sm_tile_shape = Coord(M_TILE, N_TILE); // in float4 type, sm shape is M_tile, N_tile/4 as well
+  auto out_sm_tile_shape_v = Coord(M_TILE, CEIL_DIV(N_TILE, 4)); // in float4 type, sm shape is M_tile, N_tile/4 as well
 
-  auto per_block_data_shape_v = Coord(Y_THREAD, 4*X_THREAD);
-  auto per_block_data_shape = Coord(Y_THREAD, X_THREAD);
+  auto per_block_data_shape = Coord(Y_THREAD, X_THREAD); // same threading
 
   Matrix lhs_mat = Matrix(lhs, lhs_shape);
-  Matrix rhs_mat = Matrix(rhs, rhs_shape);
-  MatrixV out_mat = MatrixV((float4*)out, Coord(M_TILE, CEIL_DIV(N_TILE, 4)));
+  MatrixV rhs_mat = MatrixV((float4*)rhs, rhs_shape_v);
+  MatrixV out_mat = MatrixV((float4*)out, out_shape_v); // use MatrixV
 
 
   Matrix lhs_shared_mat = make_shared<M_TILE, K_TILE>();
-  Matrix rhs_shared_mat = make_shared<K_TILE, N_TILE>();
-
+  MatrixV rhs_shared_mat = make_shared_v<K_TILE, CEIL_DIV(N_TILE, 4)>();
   MatrixV partial_sum =
-      make_local_v<CEIL_DIV(M_TILE, Y_THREAD), CEIL_DIV(N_TILE, 4*X_THREAD)>();
+      make_local_v<CEIL_DIV(M_TILE, Y_THREAD), CEIL_DIV(CEIL_DIV(N_TILE, 4), X_THREAD)>();
 
 
   for (int ko = 0; ko < CEIL_DIV(K, K_TILE); ko++) {
@@ -759,12 +763,12 @@ __global__ void _catzilla_matmul_v8(int M, int N, int K, float alpha,
     }
     for (int kin = 0; kin < CEIL_DIV(K_TILE, Y_THREAD); kin++) {
       #pragma unroll
-      for (int n = 0; n < CEIL_DIV(N_TILE, X_THREAD); n++) {
+      for (int n = 0; n < CEIL_DIV(CEIL_DIV(N_TILE, 4), X_THREAD); n++) {
         rhs_shared_mat
           .tile_ex(Coord(kin, n), per_block_data_shape)
           .dist_ex(Coord(threadIdx.y, threadIdx.x))
           = rhs_mat
-          .tile_ex(Coord(ko, blockIdx.x), rhs_sm_tile_shape)
+          .tile_ex(Coord(ko, blockIdx.x), rhs_sm_tile_shape_v)
           .tile_ex(Coord(kin, n), per_block_data_shape)
           .dist_ex(Coord(threadIdx.y, threadIdx.x));
       }
@@ -779,13 +783,12 @@ __global__ void _catzilla_matmul_v8(int M, int N, int K, float alpha,
 
   for (int m = 0; m < CEIL_DIV(M_TILE, Y_THREAD); m++) {
     #pragma unroll
-    for (int n = 0; n < CEIL_DIV(N_TILE, 4*X_THREAD); n++) {
+    for (int n = 0; n < CEIL_DIV(CEIL_DIV(N_TILE, 4), X_THREAD); n++) {
       out_mat
-        .tile_ex(Coord(blockIdx.y, blockIdx.x), out_sm_tile_shape)
-        .tile_ex(Coord(m, n), per_block_data_shape_v)
+        .tile_ex(Coord(blockIdx.y, blockIdx.x), out_sm_tile_shape_v)
+        .tile_ex(Coord(m, n), per_block_data_shape)
         .dist_ex(Coord(threadIdx.y, threadIdx.x)) 
-        = partial_sum
-        .dist_ex(Coord(m, n));
+        = partial_sum.dist_ex(Coord(m, n));
     }
   }
 }
