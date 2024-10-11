58f6c1bc084008b712b3e2616b7d4525191f3eac
Albert.Shi <codefisheng@gmail.com>
2024-10-11 15:58:46 +0800

    add test suites for cuda and static check; add tile tests for both runtime and compile-time check

--- catz/macro.h
+++ catz/macro.h
@@ -4,6 +4,7 @@
 namespace catz
 {
 
+// KERNEL UTILS
 #define CEIL_DIV(dividend, divisor) (((dividend) + (divisor)-1) / (divisor))
 
 #define MAKE_SHARED(matrixVar, size, type)                                     \
@@ -13,6 +14,32 @@ namespace catz
 #define MAKE_SHARED(matrixVar, size_x, size_y, type)                           \
   __shared__ type matrixVar##_data[size_x * size_y];                           \
   Matrix<type> matrixVar = Matrix<type>(matrixVar##_data, Coord(size_x, size_y))
+
+// TEST UTILS
+// static check at compile time
+#define SCHECK(cond) static_assert(cond, "compile-time check failed\n")
+#define SCHECK_FALSE(cond) static_assert(!(cond), "compile-time check failed\n")
+
+#define CONCATENATE(x, y) x##y
+
+#define CUDA_KERNEL_NAME(name) CONCATENATE(name, _cuda)
+
+// 检查 CUDA 错误的宏
+#define CUDA_CHECK_ERROR()                                                     \
+  do {                                                                         \
+    cudaError_t err = cudaGetLastError();                                      \
+    REQUIRE(err == cudaSuccess);                                               \
+    cudaDeviceSynchronize();                                                   \
+  } while (0)
+
+#define TEST_CUDA_CASE(kernel_name, case_name, tags)                           \
+  __global__ void CUDA_KERNEL_NAME(kernel_name)();                             \
+  TEST_CASE(case_name, tags)                                                   \
+  {                                                                            \
+    CUDA_KERNEL_NAME(kernel_name)<<<1, 1>>>();                                 \
+    CUDA_CHECK_ERROR();                                                        \
+  }                                                                            \
+  __global__ void CUDA_KERNEL_NAME(kernel_name)()
 
 } // namespace catz
 
--- tests/unittests/matrix_test.cu
+++ tests/unittests/matrix_test.cu
@@ -5,97 +5,83 @@
 #include <cublas_v2.h>
 #include <cuda_runtime.h>
 
+#include "macro.h"
 #include "matrix.h"
 
 using namespace catz;
 
-// TEST_CASE("matrix construction full", "[matrix][construction]") {
-//   const int M_TILE = 32;
-//   const int K_TILE = 32;
-//   auto _shape = CoordNightly<M_TILE, K_TILE>();
-//   auto _stride = CoordNightly<K_TILE, 1>();
-//   float lhs_data[M_TILE*K_TILE] = {0.0};
-//   auto lhs_mat = make_matrix(lhs_data, _shape, _stride);
-//
-//   static_assert(lhs_mat.shape.first == 32,
-//                 "matrix.shape.first not available at compile-time");
-//   static_assert(lhs_mat.shape.second == 32,
-//                 "matrix.shape.first not available at compile-time");
-//
-//   static_assert(lhs_mat.stride.first == 32,
-//                 "matrix.stride.first not available at compile-time");
-//   static_assert(lhs_mat.stride.second == 1,
-//                 "matrix.stride.first not available at compile-time");
-// }
-//
-// TEST_CASE("matrix construction contiguous", "[matrix][construction]") {
-//   const int M_TILE = 26;
-//   const int K_TILE = 32;
-//   auto _shape = CoordNightly<M_TILE, K_TILE>();
-//   float lhs_data[M_TILE*K_TILE] = {0.0};
-//   auto lhs_mat = make_matrix(lhs_data, _shape);
-//
-//   static_assert(lhs_mat.shape.first == 26,
-//                 "matrix.shape.first not available at compile-time");
-//   static_assert(lhs_mat.shape.second == 32,
-//                 "matrix.shape.first not available at compile-time");
-//
-//   static_assert(lhs_mat.stride.first == 32,
-//                 "matrix.stride.first not available at compile-time");
-//   static_assert(lhs_mat.stride.second == 1,
-//                 "matrix.stride.first not available at compile-time");
-// }
 
-// TEST_CASE("matrix tile contiguous legacy", "[matrix][tile]") {
-//   const int M_TILE = 26;
-//   const int K_TILE = 32;
-//   const int M_REG = 13;
-//   const int K_REG = 4;
-//   auto _shape = CoordNightly<M_TILE, K_TILE>();
-//   float lhs_data[M_TILE*K_TILE] = {0.0};
-//   auto lhs_mat = make_matrix(lhs_data, _shape);
-//   auto _tile_shape = CoordNightly<M_REG, K_REG>();
-//   auto rhs_mat = lhs_mat.tile(Coord(0, 0), _tile_shape);
-//
-//   static_assert(rhs_mat.shape.first == 13,
-//                 "matrix.shape.first not available at compile-time");
-//   static_assert(rhs_mat.shape.second == 4,
-//                 "matrix.shape.first not available at compile-time");
-//
-//   static_assert(rhs_mat.stride.first == 32,
-//                 "matrix.stride.first not available at compile-time");
-//   static_assert(rhs_mat.stride.second == 1,
-//                 "matrix.stride.first not available at compile-time");
-// }
+TEST_CUDA_CASE(matrix_construction_1, "matrix construction full", "[matrix][construction]") {
+  const int M_TILE = 32;
+  const int K_TILE = 32;
+  auto _shape = CoordNightly<M_TILE, K_TILE>();
+  auto _stride = CoordNightly<K_TILE, 1>();
+  float lhs_data[M_TILE*K_TILE] = {0.0};
+  auto lhs_mat = make_matrix(lhs_data, _shape, _stride);
 
-template <int M_TILE, int K_TILE, int M_REG, int K_REG>
-__global__ void matrix_tile_test_on_gpu(float* lhs_data) {
+  static_assert(lhs_mat.shape.first == 32,
+                "matrix.shape.first not available at compile-time");
+  static_assert(lhs_mat.shape.second == 32,
+                "matrix.shape.first not available at compile-time");
+
+  static_assert(lhs_mat.stride.first == 32,
+                "matrix.stride.first not available at compile-time");
+  static_assert(lhs_mat.stride.second == 1,
+                "matrix.stride.first not available at compile-time");
+}
+
+TEST_CUDA_CASE(matrix_construction_2, "matrix construction contiguous", "[matrix][construction]") {
+  const int M_TILE = 26;
+  const int K_TILE = 32;
+  auto _shape = CoordNightly<M_TILE, K_TILE>();
+  float lhs_data[M_TILE*K_TILE] = {0.0};
+  auto lhs_mat = make_matrix(lhs_data, _shape);
+
+  static_assert(lhs_mat.shape.first == 26,
+                "matrix.shape.first not available at compile-time");
+  static_assert(lhs_mat.shape.second == 32,
+                "matrix.shape.first not available at compile-time");
+
+  static_assert(lhs_mat.stride.first == 32,
+                "matrix.stride.first not available at compile-time");
+  static_assert(lhs_mat.stride.second == 1,
+                "matrix.stride.first not available at compile-time");
+}
+
+// TODO: allow for TEST_CASE style entry, no need for first identifier
+TEST_CASE("matrix tile contiguous legacy", "[matrix][tile]") {
+  const int M_TILE = 26;
+  const int K_TILE = 32;
+  const int M_REG = 13;
+  const int K_REG = 4;
+  auto _shape = Coord(M_TILE, K_TILE);
+  float lhs_data[M_TILE*K_TILE] = {0.0};
+  auto lhs_mat = Matrix(lhs_data, _shape);
+  auto _tile_shape = Coord(M_REG, K_REG);
+  auto rhs_mat = lhs_mat.tile(Coord(0, 0), _tile_shape);
+
+  CHECK(rhs_mat.shape.first == 13);
+  CHECK(rhs_mat.shape.second == 4);
+  CHECK(rhs_mat.stride.first == 32);
+  CHECK(rhs_mat.stride.second == 1);
+
+  CHECK_FALSE(rhs_mat.stride.second == 2);
+}
+
+TEST_CUDA_CASE(matrix_tile_contiguous, "matrix tile contiguous", "[matrix][tile]") {
+  const int M_TILE = 26;
+  const int K_TILE = 32;
+  const int M_REG = 13;
+  const int K_REG = 4;
+  float lhs_data[M_TILE*K_TILE] = {0.0};
   auto _shape = CoordNightly<M_TILE, K_TILE>();
 
   auto lhs_mat = make_matrix(lhs_data, _shape);
   auto _tile_shape = CoordNightly<M_REG, K_REG>();
   auto rhs_mat = lhs_mat.tile(Coord(0, 0), _tile_shape);
 
-  static_assert(rhs_mat.shape.first == 13,
-                "matrix.shape.first not available at compile-time");
-  static_assert(rhs_mat.shape.second == 4,
-                "matrix.shape.first not available at compile-time");
-
-  static_assert(rhs_mat.stride.first == 32,
-                "matrix.stride.first not available at compile-time");
-  static_assert(rhs_mat.stride.second == 1,
-                "matrix.stride.first not available at compile-time");
-}
-
-TEST_CASE("matrix tile contiguous", "[matrix][tile]") {
-  const int M_TILE = 26;
-  const int K_TILE = 32;
-  const int M_REG = 13;
-  const int K_REG = 4;
-  float lhs_data[M_TILE*K_TILE] = {0.0};
-  matrix_tile_test_on_gpu<
-    M_TILE,
-    K_TILE,
-    M_REG,
-    K_REG><<<1, 1>>>(lhs_data);
+  SCHECK(rhs_mat.shape.first == 13);
+  SCHECK(rhs_mat.shape.second == 4);
+  SCHECK(rhs_mat.stride.first == 32);
+  SCHECK(rhs_mat.stride.second == 1);
 }
