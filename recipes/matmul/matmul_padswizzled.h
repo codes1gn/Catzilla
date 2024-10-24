2f34cba70e70052ddac549f6cd389b398706c888
Albert.Shi <codefisheng@gmail.com>
2024-10-24 10:35:25 +0800

    update kernel 6

--- recipes/matmul/matmul_padswizzled.h
+++ recipes/matmul/matmul_padswizzled.h
@@ -6,6 +6,8 @@
 #include <cublas_v2.h>
 #include <cuda_runtime.h>
 
+#include "coord.h"
+#include "index.h"
 #include "macro.h"
 #include "matrix.h"
 #include "ukernels.h"
@@ -25,62 +27,50 @@ template <const int M_TILE, const int N_TILE, const int K_TILE,
 __global__ void _matmul_pad_swizzled(int M, int N, int K, float alpha,
                                      float *lhs, float *rhs, float beta,
                                      float *out) {
-  auto lhs_shape = CoordDyn(M, K);
-  auto rhs_shape = CoordDyn(K, N);
-  auto out_shape = CoordDyn(M, N);
+  auto lhs_shape = make_coord_dyn(M, K);
+  auto rhs_shape = make_coord_dyn(K, N);
+  auto out_shape = make_coord_dyn(M, N);
 
-  auto lhs_sm_tile_shape = CoordDyn(M_TILE, K_TILE);
-  auto rhs_sm_tile_shape = CoordDyn(K_TILE, N_TILE);
-  auto out_sm_tile_shape = CoordDyn(M_TILE, N_TILE);
+  auto lhs_sm_tile_shape = make_coord(M_TILE, K_TILE);
+  auto rhs_sm_tile_shape = make_coord(K_TILE, N_TILE);
+  auto out_sm_tile_shape = make_coord(M_TILE, N_TILE);
 
-  // make sure inner block looks like this, to ensure coalescing
-  auto per_block_data_shape = CoordDyn(Y_THREAD, X_THREAD);
+  auto lhs_sm_tile_shape_pad = make_coord(M_TILE, K_TILE + 1);
+  auto rhs_sm_tile_shape_pad = make_coord(K_TILE, N_TILE + 1);
 
-  MatrixDyn<float> lhs_mat = MatrixDyn<float>(lhs, lhs_shape);
-  MatrixDyn<float> rhs_mat = MatrixDyn<float>(rhs, rhs_shape);
-  MatrixDyn<float> out_mat = MatrixDyn<float>(out, out_shape);
+  auto per_block_data_shape = make_coord(Y_THREAD, X_THREAD);
 
-  // __shared__ float lhs_shared[M_TILE* (K_TILE+1)];
+  auto lhs_mat = make_matrix(lhs, lhs_shape);
+  auto rhs_mat = make_matrix(rhs, rhs_shape);
+  auto out_mat = make_matrix(out, out_shape);
 
-  MatrixDyn<float> lhs_shared_mat = make_shared<M_TILE, K_TILE + 1, float>();
-  MatrixDyn<float> rhs_shared_mat = make_shared<K_TILE, N_TILE + 1, float>();
-  // MatrixDyn<float> out_shared_mat = make_shared<M_TILE_SM, N_TILE_SM>();
-  //
+  MAKE_SHARED_MATRIX(lhs_shared_mat, lhs_sm_tile_shape_pad, float);
+  MAKE_SHARED_MATRIX(rhs_shared_mat, rhs_sm_tile_shape_pad, float);
 
-  MatrixDyn<float> partial_sum =
-      make_local<CEIL_DIV(M_TILE, Y_THREAD), CEIL_DIV(N_TILE, X_THREAD),
-                 float>();
+  MAKE_LOCAL_MATRIX(partial_sum, out_sm_tile_shape / per_block_data_shape,
+                    float);
 
-  // int threadId = threadIdx.y * X_THREAD + threadIdx.x;
-
-  for (int ko = 0; ko < CEIL_DIV(K, K_TILE); ko++) {
-    for (int m = 0; m < CEIL_DIV(M_TILE, Y_THREAD); m++) {
+  for (auto ko = I(0); ko < make_index(CEIL_DIV(K, K_TILE)); ++ko) {
+    for (auto m = I(0); m < make_index<CEIL_DIV(M_TILE, Y_THREAD)>(); ++m) {
 #pragma unroll
-      for (int kin = 0; kin < CEIL_DIV(K_TILE, X_THREAD); kin++) {
-        lhs_shared_mat.tile(CoordDyn(m, kin), per_block_data_shape)
-            .dist_to(CoordDyn(threadIdx.y, threadIdx.x))
-            // .dist_to(CoordDyn(threadId / K_TILE, threadId % K_TILE))
-            = lhs_mat.tile(CoordDyn(blockIdx.y, ko), lhs_sm_tile_shape)
-                  .tile(CoordDyn(m, kin), per_block_data_shape)
-                  // .dist_to(CoordDyn(threadId / K_TILE, threadId % K_TILE));
-                  .dist_to(CoordDyn(threadIdx.y, threadIdx.x));
-        // lhs_shared[m * Y_THREAD * (K_TILE+1) + kin * X_THREAD + threadIdx.y *
-        // (K_TILE+1) + threadIdx.x]
-        //   = lhs_mat
-        //   .tile(CoordDyn(blockIdx.y, ko), lhs_sm_tile_shape)
-        //   .tile(CoordDyn(m, kin), per_block_data_shape)
-        //   // .dist_to(CoordDyn(threadId / K_TILE, threadId % K_TILE));
-        //   .dist_to(CoordDyn(threadIdx.y, threadIdx.x)).data[0];
+      for (auto kin = I(0); kin < make_index<CEIL_DIV(K_TILE, X_THREAD)>();
+           ++kin) {
+        lhs_shared_mat.tile(CoordS(m, kin), per_block_data_shape)
+            .dist_to(CoordS(IndexDyn(threadIdx.y), IndexDyn(threadIdx.x))) =
+            lhs_mat.tile(CoordS(IndexDyn(blockIdx.y), ko), lhs_sm_tile_shape)
+                .tile(CoordS(m, kin), per_block_data_shape)
+                .dist_to(CoordS(IndexDyn(threadIdx.y), IndexDyn(threadIdx.x)));
       }
     }
-    for (int kin = 0; kin < CEIL_DIV(K_TILE, Y_THREAD); kin++) {
+    for (auto kin = I(0); kin < make_index<CEIL_DIV(K_TILE, Y_THREAD)>();
+         ++kin) {
 #pragma unroll
-      for (int n = 0; n < CEIL_DIV(N_TILE, X_THREAD); n++) {
-        rhs_shared_mat.tile(CoordDyn(kin, n), per_block_data_shape)
-            .dist_to(CoordDyn(threadIdx.y, threadIdx.x)) =
-            rhs_mat.tile(CoordDyn(ko, blockIdx.x), rhs_sm_tile_shape)
-                .tile(CoordDyn(kin, n), per_block_data_shape)
-                .dist_to(CoordDyn(threadIdx.y, threadIdx.x));
+      for (auto n = I(0); n < make_index<CEIL_DIV(N_TILE, X_THREAD)>(); ++n) {
+        rhs_shared_mat.tile(CoordS(kin, n), per_block_data_shape)
+            .dist_to(CoordS(IndexDyn(threadIdx.y), IndexDyn(threadIdx.x))) =
+            rhs_mat.tile(CoordS(ko, IndexDyn(blockIdx.x)), rhs_sm_tile_shape)
+                .tile(CoordS(kin, n), per_block_data_shape)
+                .dist_to(CoordS(IndexDyn(threadIdx.y), IndexDyn(threadIdx.x)));
       }
     }
     __syncthreads();
@@ -91,13 +81,15 @@ __global__ void _matmul_pad_swizzled(int M, int N, int K, float alpha,
   }
   __syncthreads();
 
-  for (int m = 0; m < CEIL_DIV(M_TILE, Y_THREAD); m++) {
+  for (auto m = I(0); m < make_index<CEIL_DIV(M_TILE, Y_THREAD)>(); ++m) {
 #pragma unroll
-    for (int n = 0; n < CEIL_DIV(N_TILE, X_THREAD); n++) {
-      out_mat.tile(CoordDyn(blockIdx.y, blockIdx.x), out_sm_tile_shape)
-          .tile(CoordDyn(m, n), per_block_data_shape)
-          .dist_to(CoordDyn(threadIdx.y, threadIdx.x)) =
-          partial_sum.dist_to(CoordDyn(m, n));
+    for (auto n = I(0); n < make_index<CEIL_DIV(N_TILE, X_THREAD)>(); ++n) {
+      out_mat
+          .tile(CoordS(IndexDyn(blockIdx.y), IndexDyn(blockIdx.x)),
+                out_sm_tile_shape)
+          .tile(CoordS(m, n), per_block_data_shape)
+          .dist_to(CoordS(IndexDyn(threadIdx.y), IndexDyn(threadIdx.x))) =
+          partial_sum.dist_to(CoordS(m, n));
     }
   }
 }
